a581f3c7c1f5d81fc789bec8df2c5dcba8e0d9a0
karteekkristipati8123 <karthikkristipati123@gmail.com>
2023-11-25 08:16:22 +0530

    Third Commit

--- tests/demo_codegenrator.spec.js
+++ tests/demo_codegenrator.spec.js
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.waitForURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+ 
   await page.getByPlaceholder('Username').click();
   await page.getByPlaceholder('Username').fill('Admin');
   await page.getByPlaceholder('Username').press('Tab');
